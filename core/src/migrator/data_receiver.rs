9a2240d8e5d095803e7c3937fe73abbdab58f5ea
romabliski <jakub.romaszewski@gmail.com>
2024-03-08 23:28:04 +0100

    add postgresql receiver

--- core/src/migrator/data_receiver.rs
+++ core/src/migrator/data_receiver.rs
@@ -1,7 +1,13 @@
-use crate::{db::mongodb::mongodb::Mongodb, DMResult};
+use crate::{db::{mongodb::mongodb::Mongodb, postgresql::postgresql::Postgresql}, DMResult};
 
 pub trait DataReceiver {
     async fn execute(&self) -> DMResult<()>;
+}
+
+impl DataReceiver for Postgresql {
+    async fn execute(&self) -> DMResult<()> {
+        Ok(())
+    }
 }
 
 impl DataReceiver for Mongodb {
